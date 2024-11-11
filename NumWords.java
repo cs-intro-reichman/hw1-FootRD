acbdcb1b6baf759c4b2713914c0a7b446d468140
Ron Dor <reichmanrd@gmail.com>
2024-11-11 16:45:34 +0200

    Update NumWords.Java

--- NumWords.java
+++ NumWords.java
@@ -1,12 +1,12 @@
 public class NumWords {
     public static void main(String[] args) {
         // Receive the input numbers.
-        int input_num = Integer.parseInt(args[0]);
+        int inputNum = Integer.parseInt(args[0]);
 
         // Extract hundreds, tens, and ones.
-        int hundreds = input_num / 100;
-        int tens = (input_num % 100) / 10;
-        int ones = input_num % 10;
+        int hundreds = inputNum / 100;
+        int tens = (inputNum % 100) / 10;
+        int ones = inputNum % 10;
 
         // Print the results.
         System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
